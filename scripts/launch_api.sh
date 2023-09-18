05b472f525316408b28217142b15df27648f3959
Stefano Lottini <stefano.lottini@datastax.com>
2023-09-18 17:57:49 +0200

    Strong against multiple active vector DBs

--- scripts/launch_api.sh
+++ scripts/launch_api.sh
@@ -3,7 +3,28 @@
 source /home/gitpod/.astra/cli/astra-init.sh
 clear
 astra setup
-DB_NAME=`astra db list -o json | jq -r ' .data[] | select( .V != "") | select( .Status == "ACTIVE" ) | .Name '`
+DB_NAMES=`astra db list -o json | jq -r ' .data[] | select( .V != "") | select( .Status == "ACTIVE" ) | .Name '`
+# are there exactly 1 DB?
+NUM_DBS=`echo ${DB_NAMES} | wc -w`
+if [ "${NUM_DBS}" -eq "1" ]; then
+  DB_NAME="${DB_NAMES}"
+else
+  if [ "${NUM_DBS}" -eq "0" ]; then
+    echo "ERROR: No active vector database found. Please rectify and launch this again:";
+    echo "    /workspace/langchain-flare-pdf-qa-demo/scripts/launch_api.sh";
+    exit 0;
+  else
+    echo "Choose your target vector database (enter number):";
+    select CHOSEN_DB in `echo "${DB_NAMES}"`;
+    do
+      if [ ! -z "${CHOSEN_DB}" ]; then
+        break;
+      fi
+    done
+    DB_NAME="${CHOSEN_DB}";
+  fi
+fi
+#
 astra db create-dotenv "${DB_NAME}" -d .
 /workspace/langchain-flare-pdf-qa-demo/scripts/ingest_openai_key.sh /workspace/langchain-flare-pdf-qa-demo/.env
 cd /workspace/langchain-flare-pdf-qa-demo/api
